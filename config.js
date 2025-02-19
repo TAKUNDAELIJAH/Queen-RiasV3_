a1d183db3202a8de4c47ef4775bbf1a84705e3e6
TAKUNDAELIJAH <168351848+TAKUNDAELIJAH@users.noreply.github.com>
2025-02-19 10:30:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 
 module.exports = {
     prefix: process.env.PREFIX || ".",
-    ownerName: process.env.OWNER_NAME || "Toxxic-Boy",
-    ownerNumber: process.env.OWNER_NUMBER || "2348165846414",
+    ownerName: process.env.OWNER_NAME || "King Eli",
+    ownerNumber: process.env.OWNER_NUMBER || "263781265449",
     mode: process.env.MODE || "public",
     region: process.env.REGION || "Nigeria",
     botName: process.env.BOT_NAME || "Rias Gremory V3",
     exifPack: process.env.EXIF_PACK || "RIAS V3 LOVES",
-    exifAuthor: process.env.EXIF_AUTHOR || "Toxxic",
+    exifAuthor: process.env.EXIF_AUTHOR || "Elijah",
     timeZone: process.env.TIME_ZONE || "Africa/Lagos",
     presenceStatus: process.env.PRESENCE_STATUS || "unavailable",
     autoRead: process.env.AUTO_READ === "true",
